64d20071d6dd063f635dc54ec3e9098cc313b0a2
lucas-valadares <lucasvvpaiva@hotmail.com>
2021-02-23 14:30:21 -0300

    Criado a controller de criação de usuario, e as rotas destinadas a ela

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import 'reflect-metadata'
 import express from 'express';
 import "./database";
+import { router } from './routes';
 
 const app = express();
-  
+
+app.use(express.json());
+app.use( router );
 
 app.listen(7777, () => console.log('serve is running'));
